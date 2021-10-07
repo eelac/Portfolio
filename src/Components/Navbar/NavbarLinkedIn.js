dadb1fe18944fbfa0474e1ac859858d5d96b9b81
Elton Lac <lacelton@yahoo.com>
2021-10-07 12:50:47 -0700

    Reformatted all documents

--- src/Components/Navbar/NavbarLinkedIn.js
+++ src/Components/Navbar/NavbarLinkedIn.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const NavbarLinkedIn = () => {
   return (
     <div>
-          <div
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/eltonlac/", "_blank")
-            }
-          >
-            <FontAwesomeIcon icon={faLinkedin} className="LinkedInIcon" />
-          </div>
-        </div>
+      <div
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/eltonlac/", "_blank")
+        }
+      >
+        <FontAwesomeIcon icon={faLinkedin} className="LinkedInIcon" />
+      </div>
+    </div>
   );
 };
 
