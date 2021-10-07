d82c13dc44900514581e41a50f0fdde5d7f3691e
Elton Lac <lacelton@yahoo.com>
2021-10-07 12:46:02 -0700

    Made navbar linkedin it's own component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-scroll";
 import NavbarGithub from "./NavbarGithub";
+import NavbarLinkedIn from "./NavbarLinkedIn";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import {
   faFileAlt,
   faEnvelopeOpenText,
@@ -70,15 +70,7 @@ export default class Navbar extends Component {
             <FontAwesomeIcon icon={faFileAlt} className="ResumeIcon" />
           </div>
         </div>
-        <div>
-          <div
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/eltonlac/", "_blank")
-            }
-          >
-            <FontAwesomeIcon icon={faLinkedin} className="LinkedInIcon" />
-          </div>
-        </div>
+        <NavbarLinkedIn />
         <NavbarGithub />
       </div>
     );
---
+++ src/Components/Navbar/NavbarLinkedIn.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin} from "@fortawesome/free-brands-svg-icons";
+
+const NavbarLinkedIn = () => {
+  return (
+    <div>
+          <div
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/eltonlac/", "_blank")
+            }
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="LinkedInIcon" />
+          </div>
+        </div>
+  );
+};
+
+export default NavbarLinkedIn;
