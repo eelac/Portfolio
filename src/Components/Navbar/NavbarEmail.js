020db99142455fa0232eaaf1663a5a5a3632144b
Elton Lac <lacelton@yahoo.com>
2021-10-11 10:57:55 -0700

    Email button shows text

--- src/Components/Navbar/NavbarEmail.js
+++ src/Components/Navbar/NavbarEmail.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons";
 
-const NavbarEmail = () => {
-  return (
-    <div
-      className="form-inline"
-      onClick={() =>
-        window.open(
-          "https://docs.google.com/document/d/1rRqiVPEn4KLUxdXwWwPF807MjmMOuEHkeX_SKV3URI4/edit?usp=sharing",
-          "_blank"
-        )
-      }
-    >
-      <FontAwesomeIcon icon={faEnvelopeOpenText} className="EmailIcon" />
-    </div>
-  );
-};
+class NavbarEmail extends Component {
+  state = {
+    isActive: false,
+  };
+
+  handleShow = () => {
+    this.setState({
+      isActive: true,
+    });
+  };
+
+  handleHide = () => {
+    this.setState({
+      isActive: false,
+    });
+  };
+
+  render() {
+    if (this.state.isActive) {
+      return (
+        <div className="form-inline">
+          <h1>Hello react</h1>
+          <div onClick={this.handleHide}>
+            <FontAwesomeIcon icon={faEnvelopeOpenText} className="EmailIcon" />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="form-inline">
+          <div onClick={this.handleShow}>
+            <FontAwesomeIcon icon={faEnvelopeOpenText} className="EmailIcon" />
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 export default NavbarEmail;
